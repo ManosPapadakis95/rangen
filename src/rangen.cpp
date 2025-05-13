cfa1e4ac697cfc80ba214cd2b80116ad5e60b149
Manos Papadakis <papadakm95@gmail.com>
2025-05-13 13:20:37 +0300

    Update rangen.cpp

--- src/rangen.cpp
+++ src/rangen.cpp
@@ -7,71 +7,71 @@ Rcpp::NumericVector runif(size_t n, double min = 0.0, double max = 1.0) {
 }
 
 //[[Rcpp::export(name = "Rbeta")]]
-Rcpp::NumericVector rbeta(size_t size, double alpha, double beta) {
-    return rangen::rbeta<Rcpp::NumericVector>(size, alpha, beta);
+Rcpp::NumericVector rbeta(size_t n, double alpha, double beta) {
+    return rangen::rbeta<Rcpp::NumericVector>(n, alpha, beta);
 }
 
 //[[Rcpp::export(name = "Rexp")]]
-Rcpp::NumericVector rexp(size_t size, double rate = 1.0) {
-    return rangen::rexp<Rcpp::NumericVector>(size, rate);
+Rcpp::NumericVector rexp(size_t n, double rate = 1.0) {
+    return rangen::rexp<Rcpp::NumericVector>(n, rate);
 }
 
 //[[Rcpp::export(name = "Rchisq")]]
-Rcpp::NumericVector rchisq(size_t size, double df) {
-    return rangen::rchisq<Rcpp::NumericVector>(size, df);
+Rcpp::NumericVector rchisq(size_t n, double df) {
+    return rangen::rchisq<Rcpp::NumericVector>(n, df);
 }
 
 //[[Rcpp::export(name = "Rgamma")]]
-Rcpp::NumericVector rgamma(size_t size, double shape, double rate = 1.0) {
-    return rangen::rgamma<Rcpp::NumericVector>(size, shape, rate);
+Rcpp::NumericVector rgamma(size_t n, double shape, double rate = 1.0) {
+    return rangen::rgamma<Rcpp::NumericVector>(n, shape, rate);
 }
 
 //[[Rcpp::export(name = "Rgeom")]]
-Rcpp::NumericVector rgeom(size_t size, double prob) {
-    return rangen::rgeom<Rcpp::NumericVector>(size, prob);
+Rcpp::NumericVector rgeom(size_t n, double prob) {
+    return rangen::rgeom<Rcpp::NumericVector>(n, prob);
 }
 
 //[[Rcpp::export(name = "Rcauchy")]]
-Rcpp::NumericVector rcauchy(size_t size, double location = 0.0, double scale = 1.0) {
-    return rangen::rcauchy<Rcpp::NumericVector>(size, location, scale);
+Rcpp::NumericVector rcauchy(size_t n, double location = 0.0, double scale = 1.0) {
+    return rangen::rcauchy<Rcpp::NumericVector>(n, location, scale);
 }
 
 //[[Rcpp::export(name = "Rt")]]
-Rcpp::NumericVector rt(size_t size, double df, double ncp) {
-    return rangen::rt<Rcpp::NumericVector>(size, df, ncp);
+Rcpp::NumericVector rt(size_t n, double df, double ncp) {
+    return rangen::rt<Rcpp::NumericVector>(n, df, ncp);
 }
 
 //[[Rcpp::export(name = "Rpareto")]]
-Rcpp::NumericVector rpareto(size_t size, double shape = 1.0, double scale = 1.0) {
-    return rangen::rpareto<Rcpp::NumericVector>(size, shape, scale);
+Rcpp::NumericVector rpareto(size_t n, double shape = 1.0, double scale = 1.0) {
+    return rangen::rpareto<Rcpp::NumericVector>(n, shape, scale);
 }
 
 //[[Rcpp::export(name = "Rfrechet")]]
-Rcpp::NumericVector rfrechet(size_t size, double shape = 1.0, double mean = 0.0, double scale = 1.0) {
-    return rangen::rfrechet<Rcpp::NumericVector>(size, shape, mean, scale);
+Rcpp::NumericVector rfrechet(size_t n, double shape = 1.0, double mu = 0.0, double scale = 1.0) {
+    return rangen::rfrechet<Rcpp::NumericVector>(n, shape, mu, scale);
 }
 
 //[[Rcpp::export(name = "Rlaplace")]]
-Rcpp::NumericVector rlaplace(size_t size, double mean = 0.0, double sigma = 1.0) {
-    return rangen::rlaplace<Rcpp::NumericVector>(size, mean, sigma);
+Rcpp::NumericVector rlaplace(size_t n, double mu = 0.0, double sigma = 1.0) {
+    return rangen::rlaplace<Rcpp::NumericVector>(n, mu, sigma);
 }
 
 //[[Rcpp::export(name = "Rgumblet")]]
-Rcpp::NumericVector rgumble(size_t size, double mean = 0.0, double sigma = 1.0) {
-    return rangen::rgumble<Rcpp::NumericVector>(size, mean, sigma);
+Rcpp::NumericVector rgumble(size_t n, double mu = 0.0, double sigma = 1.0) {
+    return rangen::rgumble<Rcpp::NumericVector>(n, mu, sigma);
 }
 
 //[[Rcpp::export(name = "Rarcsine")]]
-Rcpp::NumericVector rarcsine(size_t size, double min = 0.0, double max = 1.0) {
-    return rangen::rarcsine<Rcpp::NumericVector>(size, min, max);
+Rcpp::NumericVector rarcsine(size_t n, double min = 0.0, double max = 1.0) {
+    return rangen::rarcsine<Rcpp::NumericVector>(n, min, max);
 }
 
-//[[Rcpp::export(name = "Sample.int")]]
+//[[Rcpp::export(name = "Sample.int", signature = {n, size = n, replace = FALSE})]]
 Rcpp::IntegerVector sample_int(size_t n, size_t size, bool replace = false) {
     return rangen::sample<Rcpp::IntegerVector>(n, size, replace);
 }
 
-//[[Rcpp::export(name = "Sample")]]
+//[[Rcpp::export(name = "Sample", signature = {x, size, replace = FALSE})]]
 Rcpp::NumericVector sample(Rcpp::NumericVector x, size_t size, bool replace = false) {
     return rangen::sample<Rcpp::NumericVector>(x, size, replace);
 }
