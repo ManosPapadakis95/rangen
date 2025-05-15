f970ded262d9519312d5a2bccb898998746c0a85
Manos Papadakis <papadakm95@gmail.com>
2025-05-15 17:23:06 +0300

    Update distributions.h

--- inst/include/rangen/distributions.h
+++ inst/include/rangen/distributions.h
@@ -11,19 +11,25 @@ namespace rangen
 	{
 
 		template <typename T>
-		inline constexpr bool is_arma = std::is_base_of<arma::Mat<typename T::elem_type>, T>::value ||
-										std::is_base_of<arma::Col<typename T::elem_type>, T>::value ||
-										std::is_base_of<arma::Row<typename T::elem_type>, T>::value;
+		inline constexpr bool is_arma_mat = std::is_base_of<arma::Mat<typename T::elem_type>, T>::value;
+		template <typename T>
+		inline constexpr bool is_arma_vec = std::is_base_of<arma::Col<typename T::elem_type>, T>::value ||
+											std::is_base_of<arma::Row<typename T::elem_type>, T>::value;
+		template <typename T>
+		inline constexpr bool is_arma = is_arma_mat<T> || is_arma_vec<T>;
 
 		template <typename T>
-		inline constexpr bool is_rcpp = std::is_same<Rcpp::NumericMatrix, T>::value ||
-										std::is_same<Rcpp::IntegerMatrix, T>::value ||
-										std::is_same<Rcpp::CharacterMatrix, T>::value ||
-										std::is_same<Rcpp::StringMatrix, T>::value ||
-										std::is_same<Rcpp::NumericVector, T>::value ||
-										std::is_same<Rcpp::IntegerVector, T>::value ||
-										std::is_same<Rcpp::CharacterVector, T>::value ||
-										std::is_same<Rcpp::StringVector, T>::value;
+		inline constexpr bool is_rcpp_mat = std::is_same<Rcpp::NumericMatrix, T>::value ||
+											std::is_same<Rcpp::IntegerMatrix, T>::value ||
+											std::is_same<Rcpp::CharacterMatrix, T>::value ||
+											std::is_same<Rcpp::StringMatrix, T>::value;
+		template <typename T>
+		inline constexpr bool is_rcpp_vec = std::is_same<Rcpp::NumericVector, T>::value ||
+											std::is_same<Rcpp::IntegerVector, T>::value ||
+											std::is_same<Rcpp::CharacterVector, T>::value ||
+											std::is_same<Rcpp::StringVector, T>::value;
+		template <typename T>
+		inline constexpr bool is_rcpp = is_rcpp_mat<T> || is_rcpp_vec<T>;
 
 		template <class T>
 		size_t nrow(T x)
@@ -32,13 +38,17 @@ namespace rangen
 
 			size_t res;
 
-			if constexpr (is_arma<T>)
+			if constexpr (is_arma_mat<T>)
 			{
 				res = x.n_rows;
 			}
-			else if constexpr (is_rcpp<T>)
+			else if constexpr (is_rcpp_mat<T>)
 			{
 				res = x.nrow();
+			}
+			else if constexpr (!is_arma_mat<T> && !is_rcpp_mat<T>)
+			{
+				res = x.nrow;
 			}
 			return res;
 		}
@@ -50,13 +60,17 @@ namespace rangen
 
 			size_t res;
 
-			if constexpr (std::is_base_of<arma::Mat<typename T::value_type>, T>::value)
+			if constexpr (is_arma_mat<T>)
 			{
 				res = x.n_cols;
 			}
-			else if constexpr (is_rcpp<T>)
+			else if constexpr (is_rcpp_mat<T>)
 			{
 				res = x.ncol();
+			}
+			else if constexpr (!is_arma_mat<T> && !is_rcpp_mat<T>)
+			{
+				res = x.ncol;
 			}
 			return res;
 		}
@@ -71,11 +85,15 @@ namespace rangen
 
 			T res;
 
-			if constexpr (is_arma<T>)
+			if constexpr (is_arma_vec<T>)
 			{
 				res = T(size, arma::fill::none);
 			}
-			else if constexpr (is_rcpp<T>)
+			else if constexpr (is_rcpp_vec<T>)
+			{
+				res = T(size);
+			}
+			else if constexpr (!is_arma_vec<T> && !is_rcpp_vec<T>)
 			{
 				res = T(size);
 			}
@@ -92,11 +110,15 @@ namespace rangen
 
 			T res;
 
-			if constexpr (is_arma<T>)
+			if constexpr (is_arma_mat<T>)
 			{
 				res = T(nrow, ncol, arma::fill::none);
 			}
-			else if constexpr (is_rcpp<T>)
+			else if constexpr (is_rcpp_mat<T>)
+			{
+				res = T(nrow, ncol);
+			}
+			else if constexpr (!is_arma_mat<T> && !is_rcpp_mat<T>)
 			{
 				res = T(nrow, ncol);
 			}
@@ -133,10 +155,13 @@ namespace rangen
 	{
 		T res = rangen_internal::getVector<T>(size);
 
-		if constexpr(std::is_integral_v<typename T::value_type>){
-			if(x.size() == 1){
+		if constexpr (std::is_integral_v<typename T::value_type>)
+		{
+			if (x.size() == 1)
+			{
 				const size_t n = x[0];
-				for (unsigned int i = 0; i < n; ++i){
+				for (unsigned int i = 0; i < n; ++i)
+				{
 					res[i] = i + 1;
 				}
 			}
@@ -159,52 +184,51 @@ namespace rangen
 		}
 		return res;
 	}
-	
-	// template<class T, class L>
-	// T colSample(T x, size_t size, L replace){
-	// 	Assertion::has_value_type<T>::check_concept();
-	// 	Assertion::has_value_type<T>::check_concept();
-	// 	Assertion::has_col<T>::check_concept();
-	// 	Assertion::has_col<L>::check_concept();
 
-	// 	size_t n = ncol(x);
-	// 	T res = rangen_internal::getVector<T>(n);
-
-	// 	for(size_t i=0; i<n; ++i){
-	// 		if constexpr (is_arma<T>) {
-	// 			res.col(i) = sample<arma::Col<typename T::vale_type>>(x.col(i), size, replace[i]);
-	// 		} else if constexpr (is_rcpp<T> || is_custom<T>) {
-	// 			res.column(i) = sample>(x.column(i), size, replace[i]);
-	// 		}
-	// 	}
-	// }
-
-	template <class L>
-	Rcpp::NumericMatrix colSample(Rcpp::NumericMatrix x, size_t size, L replace)
+	template <class Ret, class T, class S, class L>
+	Ret colSample(T x, S size, L replace)
 	{
-		size_t n = rangen_internal::ncol(x);
-		arma::mat xx = rangen_internal::getArmaFrom(x, false);
-		Rcpp::NumericMatrix res(xx.n_rows, xx.n_cols);
-		arma::mat Res = rangen_internal::getArmaFrom(res, false);
+		const size_t n = rangen_internal::ncol(x), m = rangen_internal::nrow(x);
+		Ret res = rangen_internal::getMatrix<Ret>(n, m);
 
 		for (size_t i = 0; i < n; ++i)
 		{
-			Res.col(i) = sample<arma::colvec>(xx.col(i), size, replace[i]);
+			if constexpr (rangen_internal::is_arma_mat<T>)
+			{
+				res.col(i) = sample(x.col(i), size[i], replace[i]);
+			}
+			else if constexpr (rangen_internal::is_rcpp_mat<T>)
+			{
+				res.column(i) = sample(x.column(i), size[i], replace[i]);
+			}
+			else if constexpr (!rangen_internal::is_arma_mat<T> && !rangen_internal::is_rcpp_mat<T>)
+			{
+				res.column(i) = sample(x.column(i), size[i], replace[i]);
+			}
 		}
 		return res;
 	}
 
-	template <class L>
-	Rcpp::NumericMatrix rowSample(Rcpp::NumericMatrix x, size_t size, L replace)
+	template <class Ret, class T, class S, class L>
+	Ret rowSample(T x, S size, L replace)
 	{
-		size_t n = rangen_internal::nrow(x);
-		arma::mat xx = rangen_internal::getArmaFrom(x, false);
-		Rcpp::NumericMatrix res(xx.n_rows, xx.n_cols);
-		arma::mat Res = rangen_internal::getArmaFrom(res, false);
+		const size_t n = rangen_internal::ncol(x), m = rangen_internal::nrow(x);
+		Ret res = rangen_internal::getMatrix<Ret>(n, m);
 
-		for (size_t i = 0; i < n; ++i)
+		for (size_t i = 0; i < m; ++i)
 		{
-			Res.row(i) = sample<arma::rowvec>(xx.row(i), size, replace[i]);
+			if constexpr (rangen_internal::is_arma_mat<T>)
+			{
+				res.row(i) = sample(x.row(i), size[i], replace[i]);
+			}
+			else if constexpr (rangen_internal::is_rcpp_mat<T>)
+			{
+				res.row(i) = sample(x.row(i), size[i], replace[i]);
+			}
+			else if constexpr (!rangen_internal::is_arma_mat<T> && !rangen_internal::is_rcpp_mat<T>)
+			{
+				res.row(i) = sample(x.row(i), size[i], replace[i]);
+			}
 		}
 		return res;
 	}
